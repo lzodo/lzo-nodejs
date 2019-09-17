87108bc321733f5a35d4d1f4a703f29c124c6989
liaozhongxun <869664233@qq.com>
2019-09-17 17:40:42 +0800

    updata

---
+++ nodedoc/model/fs/write-fs.js
@@ -0,0 +1,19 @@
+'use strict';
+var fs = require('fs');
+
+// 文件路径相对 process.cwd() 也就是打开这个项目的跟文件夹
+
+var writeData = '写入一段类容';
+//异步写入文件
+fs.writeFile('./nodedoc/model/output.txt', writeData, function (err) {
+    console.log(err); //null
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('ok.');
+    }
+});
+
+//异步写入文件（多了Sync && 不支持回调）
+var data = 'Hello, Node.js';
+fs.writeFileSync('./nodedoc/model/output.txt', data)
---
+++ nodedoc/model/stream/stream.js
@@ -0,0 +1,38 @@
+// stream 流: 输入流（stdin） 输出流（stdout） ....
+
+'use strict';
+
+var fs = require('fs');
+
+// 打开一个文件输出流:
+var rs = fs.createReadStream('./nodedoc/model/text.txt', 'utf-8');
+
+rs.on('data', function (chunk) {  //读取每一个
+    console.log('DATA:')
+    console.log(chunk);
+});
+
+rs.on('end', function () { //读取完成
+    console.log('END');
+});
+
+rs.on('error', function (err) {
+    console.log('ERROR: ' + err);
+});
+
+
+// 创建输入流
+var ws1 = fs.createWriteStream('./nodedoc/model/output1.txt', 'utf-8');
+ws1.write('使用Stream写入文本数据...\n');
+ws1.write('END.');
+ws1.end();
+
+
+
+var rsping = fs.createReadStream('./nodedoc/model/copied.txt');
+var wsping = fs.createWriteStream('./nodedoc/model/copied.txt');
+
+wsping.write('before rsping filee \n');
+
+rsping.pipe(wsping);
+
