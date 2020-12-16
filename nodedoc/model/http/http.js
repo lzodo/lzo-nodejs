7d27938eea1bfa1a859aca4aec2a61d018471c3c
liaozhongxun <869664233@qq.com>
2020-12-16 21:46:52 +0800

    简易爬虫

---
+++ nodedoc/model/http/http.js
@@ -0,0 +1,46 @@
+// // const http = require("http");
+// const http = require("https");
+// const fs = require("fs");
+// const cheerio = require("cheerio");
+
+// let url = "https://www.qunar.com";
+// http.get(url, (res) => {
+//     // 安全判断
+//     const { statusCode } = res; //获取状态码
+//     const contentType = res.headers["content-type"]; //获取文件类型
+//     console.log(contentType);
+//     console.log(statusCode);
+
+//     let err = null;
+//     if (statusCode !== 200) {
+//         err = new Error("请求状态错误");
+//     } else if (!/^text\/html/.test(contentType)) {
+//         err = new Error("文件类型错误");
+//     }
+
+//     if (err) {
+//         res.resume(); //重置缓存
+//         throw err;
+//         return false;
+//     }
+
+//     // 数据处理
+//     let linkdata = "";
+//     res.on("data", (chunk) => {
+//         // 数据分段,只要接收数据就会触发data，chunk是每次接收数据的片段,数据流
+//         linkdata += chunk;
+//         console.log("接收数据");
+//     });
+
+//     //数据传输完毕
+//     res.on("end", (chunk) => {
+//         fs.writeFileSync(__dirname + "/bilibil.html", linkdata);
+//         console.log("接收数据结束");
+//         let $ = cheerio.load(linkdata); //将请求到的内容转换为类DOM
+//         $("img").each((index, item) => {
+//             console.log($(item).attr("src"));
+//         });
+//     });
+// }).on("error", (err) => {
+//     console.log("请求错误");
+// });
--- nodedoc/model/queryString/qs.js
+++ nodedoc/model/queryString/qs.js
@@ -7,3 +7,9 @@ console.log(qs.stringify(object, "@", "=>")); //name=>lzoxun@age=>20 默认"&", 
 
 console.log(qs.escape(str)); //name%3Dlzoxun%26age%3D20  将正常字符串编码操作
 console.log(qs.unescape("name%3Dlzoxun%26age%3D20")); //name=lzoxun&age=20  解码
+
+let err = new Error("错误对象");
+console.log("111");
+
+throw err; //适当时机抛出
+console.log("222");
