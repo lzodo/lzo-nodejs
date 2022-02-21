f466879e69fad05df7a5f79d74d222f426b0c0ca
lenovo-kali <869664233@qq.com>
2022-02-22 01:04:15 +0800

    updat

--- nodedoc/model/global/global.js
+++ nodedoc/model/global/global.js
@@ -3,7 +3,7 @@
 
 
 // 常见的全部对象
-cosnole.log(process)  // node运行文件会开启一个进程，process是进程相关信息对象
+console.log(process)  // node运行文件会开启一个进程，process是进程相关信息对象
 console.log(console)
 
 setImmediate(()=>{}); //立即执行，定时器还有setInterval、clearInterval、setTimeout、clearTimeout
@@ -17,6 +17,17 @@ Object.keys(global).forEach(function(i){
 console.log(__dirname)  //文件所在文件夹绝对路径,相当 path.dirname()，非全局 global先找不到这个
 console.log(__filename) //文件的绝对路径，非全局 global先找不到这个
 // exports module require()
+
+
+/*
+ * module 查看当前模块状态
+ *    children:require进的子模块
+ *    loaded:模块是否被加载过,是的话变true,下次使用到此模块时回去缓存中获取,不会价值第二遍了
+ *    path:引入模块是的位置,优先级一次递减
+ */
+
+console.log(module)
+
 
 // 对比浏览器
 // 浏览器最顶层定义的东西就是全局的变量或函数,node有模块的概念，最顶层的只是属于该模块，不会被挂到global
