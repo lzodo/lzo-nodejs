22a4b1c6070ec67591d4353f54feecebf058929f
liaozhongxun <869664233@qq.com>
2022-03-16 17:26:12 +0800

    save every day

--- nodedoc/model/http/图片上传.js
+++ nodedoc/model/http/图片上传.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
-const qs = require("querystring")
+const qs = require("querystring");
+const path = require("path")
 
 //直接读取上传的文件流，并写入文件是不对的，因为文件流中不仅包括文件内容，还有boundary(数据分隔符号)、文件名称、属性类型等其他东西是不能写入的
 /**
@@ -18,36 +19,46 @@ http.createServer((req, res) => {
     if (req.url == "/upload") {
         req.setEncoding("binary");
 
-        let body = ""
-        let boundary = qs.parse(req.headers['content-type'],";","=");
-        console.log(boundary[' boundary'])
+        let body = "";
+        let boundary = qs.parse(req.headers["content-type"], ";", "=");
+        let fileSize = req.headers['content-length']
+        console.log(boundary[" boundary"]);
+
         req.on("data", (data) => {
             body += data;
-        })
+        });
         req.on("end", () => {
-            console.log(body)
-            let payload = qs.parse(body, "\r\n", ":");
+            console.log(body);
+            console.log('=================222');
+            let payload = qs.parse(body, "\r\n", ":"); //解析body，为了达到文件类型位置
+
+            let other_info = qs.parse(payload['Content-Disposition'], ";", "=");
+            let exp = path.extname(other_info[' filename'])
+            
             let type = payload["Content-Type"]; //获取文件类型
-            let typeIndex = body.indexOf(type);
-            let fileData = body.substring(typeIndex + type.length)
-            let reg = new RegExp(`^\\s*`);
-            let reg2 = new RegExp(`--${boundary[' boundary']}--\\s*$`);
-            console.log(reg)
-            console.log(reg2)
-            fileData=fileData.replace(reg,"");
-            fileData=fileData.replace(reg2,"");
-            console.log('fileDat===========================================a');
+            let fileData = body.substring(body.indexOf(type) + type.length); //文件内容为图片类型后面
 
-            console.log(fileData);
+            // let reg = new RegExp(`^\\s*`); //不包括前面空格与后面boundary的部分
+            // let reg2 = new RegExp(`--${boundary[" boundary"]}--\\s*$`);
+            // console.log(reg);
+            // console.log(reg2);
+            // fileData = fileData.replace(reg, "");
+            // fileData = fileData.replace(reg2, "");
 
-            fs.writeFile("./1112.png",fileData,'binary',(err)=>{
-                console.log("数据上传完毕")
-            })
-        })
+            let reg = new RegExp(`^\\s*`); //不包括前面空格与后面boundary的部分,测试后面boundary可用保留
+            console.log(reg);
+            fileData = fileData.replace(reg, "");
+
+            // 写入到文件中
+            let fileName = path.resolve(__dirname,`../testfiles/${new Date().getTime()}${exp}`)
+            fileName = fileName.replace("\"","");
+            fs.writeFile(fileName, fileData, "binary", (err) => {
+                console.log("数据上传完毕");
+            });
+        });
 
         res.end();
     }
-
-}).listen(8888, () => {
-    console.log("启动图片上传服务")
-})
+}).listen(8889, () => {
+    console.log("启动图片上传服务");
+});
