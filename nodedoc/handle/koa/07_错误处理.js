b4f0b709a112a3d833dd16b9ce446fedd0f739c2
lenovo-kali <869664233@qq.com>
2022-10-25 23:45:25 +0800

    2022-10-25 23:45提交上传

--- nodedoc/handle/koa/07_错误处理.js
+++ nodedoc/handle/koa/07_错误处理.js
@@ -2,16 +2,16 @@ const Koa = require("koa"); // 导出一个类
 
 const app = new Koa();
 
-app.use((ctx, next) => { 
-    ctx.app.emit('error', new Error("出错了"), ctx)
+app.use((ctx, next) => {
+    return ctx.app.emit('error', new Error("出错了fdsf"), ctx)
 })
 
 
 app.on("error", (err, ctx) => {
-    ctx.status = 401; 
+    ctx.status = 401;
     ctx.body = err.message;
 })
 
-app.listen("8000", () => {
+app.listen("8001", () => {
     console.log("服务器开启成功")
 })
