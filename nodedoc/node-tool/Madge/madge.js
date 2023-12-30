b5f07618eb719302bfadb8dbc4a9198560e2c243
lzodo <869664233@qq.com>
2023-12-31 00:58:46 +0800

    2023-12-31 00:58提交上传

--- nodedoc/node-tool/Madge/madge.js
+++ nodedoc/node-tool/Madge/madge.js
@@ -6,8 +6,22 @@
  */
 
 // 代码中使用
-const madge = require("madge");
+// const madge = require("madge");
 
-madge("/opt/lzo-project/lzo-tsvue-admin/src/main.ts").then((res) => {
-  console.log(res.leaves());
-});
+// madge("/opt/lzo-project/lzo-tsvue-admin/src/main.ts").then((res) => {
+//   console.log(res.leaves());
+// });
+const { exec } = require("child_process");
+
+exec(
+  "madge --image graph.png /opt/lzo-project/lzo-tsvue-admin/src/main.ts",
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`错误信息：${stderr}`);
+      return;
+    }
+
+    // 输出命令执行结果
+    console.log(`命令执行结果：\n${stdout}`);
+  }
+);
