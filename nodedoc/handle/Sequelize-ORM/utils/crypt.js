8c255cb6c3e9a8b3f9482114d02b112c7bab98bc
lzodo <869664233@qq.com>
2025-03-01 17:03:42 +0800

    优化

--- nodedoc/handle/Sequelize-ORM/utils/crypt.js
+++ nodedoc/handle/Sequelize-ORM/utils/crypt.js
@@ -6,7 +6,9 @@ const secret = Buffer.from("2c6a07aa9c43abdc");
 // console.log(result); // 查看 crypto 提供的所有加密算法
 
 // 2、准备一个随机向量，随机变化的iv，每次导出都不一样，内部方法使用的是相同的
-const iv = crypto.randomBytes(16);
+// 加解密的iv必须一致，后期可以通过固定iv，或将iv储存，解密再获取的方式保持一致
+// const iv = crypto.randomBytes(16);
+const iv = Buffer.from("4f6a0gaa9c43aaaa");
 
 // 加密一个字符串
 exports.encrypt = function (str) {
