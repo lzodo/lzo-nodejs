0a70ba8e0e3b0c86b20d569af95d9d5bec32f329
lzodo <869664233@qq.com>
2025-03-02 10:30:11 +0800

    redis与session

--- nodedoc/handle/Sequelize-ORM/index.js
+++ nodedoc/handle/Sequelize-ORM/index.js
@@ -9,10 +9,12 @@ const useRouter = require("./routes/index");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const { RedisStore } = require("connect-redis");
 const { authByCookie, authBySession } = require("./middleware/auth");
 const { crosVis } = require("./middleware/cros");
 const errHealder = require("./middleware/error");
-require("./redis");
+const client = require("./redis");
+client.select(2);
 
 /**
  * 静态资源服务器
@@ -68,12 +70,19 @@ app.use(
   session({
     secret: "miyao",
     name: "sessionId",
+    resave: true, // 强制保存到仓库
+    saveUninitialized: false, // 没有用过的session要不要保存
     cookie: {
       // session 内部也是需要通过cookie实现的
       maxAge: 60 * 60 * 1000,
       base: "/",
       domain: "localhost",
     },
+    // 如果不用仓库，存在内存，数据一会就丢失了
+    store: new RedisStore({
+      client: client,
+      prefix: "sessionLogin:",
+    }),
   })
 );
 app.use(authBySession());
