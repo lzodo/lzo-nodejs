f879c0564e0abf5ee336534d74157122560e0483
lzodo <869664233@qq.com>
2025-03-08 20:20:51 +0800

    信号中断关闭数据库关闭

--- nodedoc/handle/Sequelize-ORM/src/index.js
+++ nodedoc/handle/Sequelize-ORM/src/index.js
@@ -4,9 +4,33 @@ require('./models/sync'); // 初始化模型
 
 const { servers } = require('./config');
 const app = require('./app');
+const sequelize = require('./models/db');
+const client = require('./redis');
 
 // 监听一个服务
 const { port } = servers;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`server listen on ${port}`);
+});
+
+// SIGTERM：终止信号。通常由系统或进程管理器（如 PM2、Kubernetes）发送，要求应用优雅关闭。
+process.on('SIGTERM', () => {
+	console.log('接收到程序终止信号 ...');
+	server.close(async () => {
+		await sequelize.close(); // 关闭 Sequelize 连接
+		await client.quit();
+		console.log('Server closed.');
+		process.exit(0);
+	});
+});
+
+// SIGINT：中断信号。通常由用户按下 Ctrl+C 触发，要求应用立即关闭。
+process.on('SIGINT', () => {
+	console.log('接收到服务中断信号 ...');
+	server.close(async () => {
+		await sequelize.close(); // 关闭 Sequelize 连接
+		await client.quit();
+		console.log('Server closed.');
+		process.exit(0);
+	});
 });
