5219c9aaeb268089d274a429bdd292243317655d
lzodo <869664233@qq.com>
2025-03-27 20:53:05 +0800

    feat(NestJS): typeORM 一对一、一对多关联关系

--- nodedoc/handle/nestjs/case/nest-demo/src/user/entity/profile.entity.ts
+++ nodedoc/handle/nestjs/case/nest-demo/src/user/entity/profile.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from './user.entity';
 
 @Entity()
 export class Profile {
@@ -13,4 +20,14 @@ export class Profile {
 
   @Column()
   address: string;
+
+  /**
+   * 建立用户信息表和用户表的一对一关系
+   * 1、OneToOne 参数使用函数，可以在需要的时候调用
+   * 2、JoinColumn 再单前表创建管理的字段（外键）
+   * 3、user: User; 将匹配时数据注入到 user 这个属性中（sequelize 关联关系中的别名）
+   */
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'userId' }) // 不指定外键名，默认也是小驼峰
+  user: User;
 }
--- nodedoc/handle/nestjs/case/nest-demo/src/user/entity/user.entity.ts
+++ nodedoc/handle/nestjs/case/nest-demo/src/user/entity/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Logs } from 'src/logs/entity/logs.entity';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 
 // 指定表名（可选，默认使用类名小写）
 @Entity()
@@ -14,4 +15,12 @@ export class User {
   // 普通列
   @Column()
   password: string;
+
+  /**
+   * 创建于日志的一对多关系
+   *   (logs) => logs.user 让数据库查询用户的时候，去查logs表，把这个用户的日志信息，塞到属性logs（第二行key）中一起查询
+   *             logs.user  就是去logs表找userId=当前id的记录
+   */
+  @OneToMany(() => Logs, (logs) => logs.user)
+  logs: Logs;
 }
