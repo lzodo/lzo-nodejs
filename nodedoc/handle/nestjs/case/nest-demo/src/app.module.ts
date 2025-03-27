8a4dc953f2f737d2a11f684ce038e24b121c7ea5
lzodo <869664233@qq.com>
2025-03-27 19:00:36 +0800

    feat(NestJS): typeORM连接MySql

--- nodedoc/handle/nestjs/case/nest-demo/src/app.module.ts
+++ nodedoc/handle/nestjs/case/nest-demo/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { RangerModule } from './ranger/ranger.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import Configuration from './config/configuration';
 import * as Joi from 'joi';
+import { ConfigEnum } from './enum/config.enum';
 
 /**
  * nestjs 中所有东西都与模块相关联，所有服务，路由都是模块的分支
@@ -23,7 +25,32 @@ import * as Joi from 'joi';
       // 环境变量校验
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('development', 'production'),
+
+        DB_TYPE: Joi.string().valid('mysql', 'mariadb'),
+        DB_HOST: Joi.string().ip().required(),
+        DB_PORT: Joi.number().default(3306),
+        DB_ACCOUNT: Joi.string().required(),
+        DB_PASSWD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        DB_SYNC: Joi.boolean().default(false),
       }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: configService.get(ConfigEnum.DB_TYPE),
+          host: configService.get(ConfigEnum.DB_HOST),
+          port: configService.get(ConfigEnum.DB_PORT),
+          username: configService.get(ConfigEnum.DB_ACCOUNT),
+          password: configService.get(ConfigEnum.DB_PASSWD),
+          database: configService.get(ConfigEnum.DB_DATABASE),
+          entities: [],
+          // 同步，不应在生产中使用 - 否则你可能会丢失生产数据。
+          synchronize: configService.get(ConfigEnum.DB_SYNC),
+          logging: ['error'],
+        }) as TypeOrmModuleOptions,
     }),
     UserModule,
     RangerModule,
