ebde3d0298c09172c81fe6999185751ea63c1481
lzodo <869664233@qq.com>
2025-03-27 11:24:04 +0800

    feat(NestJS): joi校验环境变量

--- nodedoc/handle/nestjs/case/nest-demo/src/app.module.ts
+++ nodedoc/handle/nestjs/case/nest-demo/src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { RangerModule } from './ranger/ranger.module';
 import { ConfigModule } from '@nestjs/config';
 import Configuration from './config/configuration';
+import * as Joi from 'joi';
 
 /**
  * nestjs 中所有东西都与模块相关联，所有服务，路由都是模块的分支
@@ -13,9 +14,16 @@ import Configuration from './config/configuration';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // 全局使用，其他模块使用就不需要 imports 中配置 ConfigModule 了
-      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'], // 前面的优先级高，.env 的配置进行共享
-      load: [Configuration], // 使用yml配置文件复杂数据
+      // 全局使用，其他模块使用就不需要 imports 中配置 ConfigModule 了
+      isGlobal: true,
+      // 前面的优先级高，.env 的配置进行共享
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
+      // 如果使用yml配置文件复杂数据
+      load: [Configuration],
+      // 环境变量校验
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production'),
+      }),
     }),
     UserModule,
     RangerModule,
